15a625e909ac3be7978665c9a8ada17f9320b2f6
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-13 22:31:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ function lowerCaseDrivers(drivers) {
 }
 
 function nameToAttributes() {
-  
+  return drivers.map((element) => {
+    name = element.split(" ");
+    return {firstName: name[0], lastName: name[1]};
+  });
 }
 
 function attributesToPhrase() {
-  
+
 }
