d2dfaba3d69236f397151bd03c0eb7c3209dc82b
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-13 22:32:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function lowerCaseDrivers(drivers) {
   return drivers.map((element) => element.toLowerCase());
 }
 
-function nameToAttributes() {
+function nameToAttributes(drivers) {
   return drivers.map((element) => {
     name = element.split(" ");
     return {firstName: name[0], lastName: name[1]};
