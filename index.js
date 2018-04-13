bed9dae1f1ef126ca6f3d192b2c78941f56a5644
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-13 22:24:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map((element) => element.toLowerCase());
+}
+
+function nameToAttributes() {
+  
+}
+
+function attributesToPhrase() {
+  
+}
